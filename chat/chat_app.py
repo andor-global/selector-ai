b63529c49b1a79d93a36bbdc15b9ec85bd0ee330
alesanm <alesan.med@yandex.ru>
2023-11-15 18:43:37 +0100

    add memory to llm chain

--- chat/chat_app.py
+++ chat/chat_app.py
@@ -5,15 +5,36 @@ import requests
 os.environ["REPLICATE_API_TOKEN"] = st.secrets['REPLICATE_API_TOKEN']
 
 from langchain.prompts import PromptTemplate
-llm_prompt = PromptTemplate.from_template(
-    "You are experineced personal stylist. Please, describe appropriate style look taking into account that info: {text}?"
-)
+# llm_prompt = PromptTemplate.from_template(
+# )
 
-from langchain.memory import ConversationBufferWindowMemory
-memoryT = ConversationBufferWindowMemory(k=5)
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
+
+llm_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are experineced personal stylist. Please, describe appropriate style look taking into account that info: "),
+        MessagesPlaceholder(variable_name="history"),
+        ("human", "{text}"),
+    ]
+)
 
 from langchain.llms import Replicate
 llm = Replicate(model="meta/llama-2-7b-chat:8e6975e5ed6174911a6ff3d60540dfd4844201974602551e10e9e87ab143d81e")
+
+from operator import itemgetter
+memory = ConversationBufferMemory(return_messages=True)
+memory.load_memory_variables({})
+
+llm_chain = (
+    RunnablePassthrough.assign(
+        history=RunnableLambda(memory.load_memory_variables) | itemgetter("history")
+    )
+    | llm_prompt
+    | llm
+)
+
 
 image_prompt = PromptTemplate(
     input_variables=["style look description"],
@@ -22,7 +43,7 @@ image_prompt = PromptTemplate(
 text2image = Replicate(model="stability-ai/sdxl:39ed52f2a78e934b3ba6e2a89f5b1c712de7dfea535525255b1aa35c5565e08b")
 
 from langchain.schema.runnable import RunnablePassthrough
-llm_chain = llm_prompt | llm
+# llm_chain = llm_prompt | llm
 image_chain = image_prompt | text2image
 final_chain = {"style look description": llm_chain} | RunnablePassthrough.assign(image=image_chain)
 
@@ -38,6 +59,12 @@ while(True):
     # parse the output
     style_look_description = chain_output['style look description']
     image_url = chain_output['image']
+
+    print('bot :', style_look_description)
+
+    # update llm_chain history
+    memory.save_context({"input": user_input}, {"output": style_look_description})
+    print(memory.load_memory_variables({}))
 
     # get the image in bytes
     image_bytes = (requests.get(image_url)).content
