258dc83b0c7bb194e9949e88f5fbda1ce66879e8
alesanm <alesan.med@yandex.ru>
2023-11-30 11:23:55 +0100

    simplify prompt for the llm

--- chat/prompt.py
+++ chat/prompt.py
@@ -13,32 +13,8 @@ def create_llm_prompt():
     - ChatPromptTemplate: LangChain prompt template for a personal stylist conversation.
     """
     # Define a system message template for the personal stylist role
-    system_message_template = (
-        "Age: {age}. Gender: {gender}. "
-        "Psychotype description: {psychotype_description}."
-        "You are an experienced personal stylist. Given the following description of my personality, "
-        "please provide me a textual description of the style look. Just a set of items to wear, no more."
-    )
+    prompt = PromptTemplate.from_template("I'm {age} years old. I'm a {gender}. My personality description is {psychotype_description}. Please, take into account new additional info: {text}")
 
-    # Create a system message prompt template
-    system_message_prompt = SystemMessagePromptTemplate.from_template(system_message_template)
-
-    # Define a human message template with placeholders for user profile information
-    human_message_template = (
-        "Please, take into account new additional info: {text}"
-    )
-
-    # Create a human message prompt template
-    human_message_prompt = HumanMessagePromptTemplate.from_template(human_message_template)
-
-    # Create a chat prompt template with system message, message history placeholder, and human message
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            system_message_prompt,
-            MessagesPlaceholder(variable_name="history"),
-            human_message_prompt
-        ]
-    )
     return prompt
 
 def create_text2image_prompt():
