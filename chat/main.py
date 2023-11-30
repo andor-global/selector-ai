5fb21bd3587c9d36b4f4240e3ff58fadb53b786b
alesanm <alesan.med@yandex.ru>
2023-11-30 17:58:40 +0100

    replace modules by chat class

--- chat/main.py
+++ chat/main.py
@@ -1,67 +1,15 @@
-from PIL import Image
-import requests
 import os
-from chat.chain import create_chain
-from chat.memory import create_memory
-import io
+from chat import Chat
 
+def main(user_profile_info, memory_state={}):
+    # Entry point for the chatbot application
+    os.environ["REPLICATE_API_TOKEN"] = ...
 
-def chat(user_profile_info, memory_state={}):
-    """
-    Conducts a chat using the Replicate API and displays information including style descriptions and images.
-
-    Parameters:
-    - user_profile_info (dict): User profile information containing gender, age, and psychotype description.
-    - memory_state (dict): Optional memory state to initialize the conversation memory.
-
-    Example Usage:
-    user_profile_info = {'gender': 'female', 'age': '27', 'psychotype_description': 'dramatic'}
-    chat(user_profile_info)
-    """
-
-    # Initialize Replicate API token from Streamlit secrets
-    os.environ["REPLICATE_API_TOKEN"] = st.secrets['REPLICATE_API_TOKEN']
-
-    # Initialize conversation memory
-    memory = create_memory(memory_state={})
-
-    # Create conversation chain
-    chain = create_chain(memory)
-
-    # Start the conversation loop
-    while True:
-        # Get user input from the console
-        user_input = input('user: ')
-
-        # Invoke the chain with user input and user profile information
-        chain_output = chain.invoke({
-            'text': user_input,
-            'gender': user_profile_info['gender'],
-            'age': user_profile_info['age'],
-            'psychotype_description': user_profile_info['psychotype_description']
-        })
-
-        # Extract style look description and image URL from chain output
-        style_look_description = chain_output['image_description']
-        image_url = chain_output['image']
-
-        # Display the bot's response
-        print('bot:', style_look_description)
-
-        # Save the conversation context to memory
-        memory.save_context({"input": user_input}, {"output": style_look_description})
-        print(memory.load_memory_variables({}))
-
-        # Get the image in bytes from the provided URL
-        image_bytes = requests.get(image_url).content
-
-        # Convert bytes to a PIL Image
-        image = Image.open(io.BytesIO(image_bytes))
-
-        # Save the image locally
-        image.save("images\image.jpg")
-
+    # Create an instance of the Chat class and execute the chat loop
+    chat = Chat(memory_state={}, user_profile_info = user_profile_info)
+    chat.execute()
 
 # Example Usage:
 user_profile_info = {'gender': 'female', 'age': '27', 'psychotype_description': 'dramatic'}
-chat(user_profile_info)
+
+main(user_profile_info)
