87220764218d518262d65a00c3eca98c346aed3b
alesanm <alesan.med@yandex.ru>
2023-11-30 18:14:54 +0100

    rm api key

--- machine_learning/mistral_model.py
+++ machine_learning/mistral_model.py
@@ -3,7 +3,7 @@ import replicate
 
 def query(model_params):
     try:
-        replicate_ = replicate.Client(api_token='r8_PKF16BOJySVj5Qmhn6PvWKkqv6tW47J2kiCO1')
+        replicate_ = replicate.Client(api_token=)
         output = replicate_.run(
             "nateraw/mistral-7b-openorca:7afe21847d582f7811327c903433e29334c31fe861a7cf23c62882b181bacb88",
             model_kwargs=model_params)
