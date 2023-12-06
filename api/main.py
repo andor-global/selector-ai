ba71f008aa7330530b1f4a741b039e24e30a8e8c
Hesam Zakeri <hesam.zk.81@gmail.com>
2023-12-06 13:53:13 +0100

    allow all links through CORS

--- api/main.py
+++ api/main.py
@@ -15,7 +15,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[os.getenv("CLIENT_DEPLOYMENT")],
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
